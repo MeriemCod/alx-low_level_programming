76a9b32ce8d7d147e13fa26117643bd5529b949d
MeriemCod <smeriem108@gmail.com>
2023-03-16 18:12:42 -0700

    Task 4, Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-*main -entry point
-*return: Always 0 (Success)
+* main - A program that prints a line with puts function
+* Return: Always 0 (Success)
 */
 int main(void)
 {
