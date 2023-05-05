46b326ef707b4852b1c7a8bf7f3fba0bc3f0d58a
MeriemCod <smeriem108@gmail.com>
2023-05-04 18:13:48 -0700

    Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,9 +9,7 @@
 void print_binary(unsigned long int n)
 	{
 		if (n > 1)
-			/*right shift operator*/
-			/*shifts all bits towards right by certain numbf 1 bits*/
-		       print_binary (n >> 1);
+			print_binary(n >> 1);
 
 	_putchar ((n & 1) + '0');
 }
