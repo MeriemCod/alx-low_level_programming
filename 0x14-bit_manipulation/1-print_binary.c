5a65543f10a10e543f8fd9bad433150b91aee176
MeriemCod <smeriem108@gmail.com>
2023-05-04 18:20:28 -0700

    Task 1 up-date

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 
 /**
- * print _binary - converts unsigned int to binary
- * @n: unsigned int
- * Return: binary
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
  */
 void print_binary(unsigned long int n)
-	{
+{
 		if (n > 1)
 			print_binary(n >> 1);
 
